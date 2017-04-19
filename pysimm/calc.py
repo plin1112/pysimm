2d3a6d22fd449b8cfdba54b9297e6b61a75b6819
Mike Fortunato <mef231@gmail.com>
2017-04-19 18:33:31 +0000

    PysimmError is in utils

--- pysimm/calc.py
+++ pysimm/calc.py
@@ -39,7 +39,7 @@ from pysimm import error_print
 from pysimm import warning_print
 from pysimm import verbose_print
 from pysimm import debug_print
-from pysimm import PysimmError
+from pysimm.utils import PysimmError
 from pysimm.utils import Item
 from pysimm.utils import ItemContainer
 
